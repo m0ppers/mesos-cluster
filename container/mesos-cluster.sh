35ea57ad012f7065b52ff8f7efd9c46603c9217f
Andreas Streichardt <andreas@arangodb.com>
2016-01-25 19:14:12 +0100

    Dynamic port allocation

--- container/mesos-cluster.sh
+++ container/mesos-cluster.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 if [ $# -lt 3 ]; then
-  echo "Usage $0 <num-slaves> <hostip> <cluster-base-dir>"
+  echo "Usage $0 <num-slaves> <hostip> <port-range> <cluster-base-dir>"
   exit 99
 fi
 
+NUM_SLAVES=$1
 HOSTIP=$2
+PORT_RANGE=$3
 # strip any trailing slashes. this is actually needed. mesos ui will not be able to access the files otherwise :S
-CLUSTER_BASE_DIR=$(echo $3 | sed -e 's/\/*$//g')
+CLUSTER_BASE_DIR=$(echo $4 | sed -e 's/\/*$//g')
 
 mountpoint $CLUSTER_BASE_DIR > /dev/null
 
@@ -19,8 +21,6 @@ fi
 HOSTNAME=`hostname`
 CLUSTER_WORK_DIR=$CLUSTER_BASE_DIR/$HOSTNAME
 IP=$(hostname --ip-address)
-
-
 
 /etc/init.d/zookeeper start
 
@@ -40,19 +40,23 @@ EOF
 
 SLAVE_RESOURCES=$(/distribute-slave-resources $1 $CLUSTER_BASE_DIR/$HOSTNAME)
 
-let slave_port=31000
-for i in `seq $1`; do
+let slave_start_port=${PORT_RANGE%:*}
+let slave_end_port=${PORT_RANGE#*:}
+let num_ports=(slave_end_port-slave_start_port)/NUM_SLAVES
+
+for i in `seq $NUM_SLAVES`; do
   SLAVE_DIR=$CLUSTER_WORK_DIR/mesos-slave-"$i"
   mkdir -p $SLAVE_DIR
-  let slave_resource_start_port=slave_port+1
-  let slave_resource_end_port=slave_resource_start_port+998
+  let slave_resource_start_port=slave_start_port+1
+  let slave_resource_end_port=slave_start_port+num_ports-1
+
   iptables -t nat -A PREROUTING -p tcp -d $IP --dport $slave_resource_start_port:$slave_resource_end_port -j DNAT --to-destination $HOSTIP
   iptables -t nat -A OUTPUT -p tcp -d $IP --dport $slave_resource_start_port:$slave_resource_end_port -j DNAT --to-destination $HOSTIP
   cat << EOF >/etc/supervisor/conf.d/mesos-slave-"$i".conf
 [program:mesos-slave-$i]
-command=mesos-slave --no-hostname_lookup --master=zk://$IP:2181/mesos --containerizers=docker --port=$slave_port --work_dir=$SLAVE_DIR --resources="$SLAVE_RESOURCES";ports(*):[$slave_resource_start_port-$slave_resource_end_port]
+command=mesos-slave --no-hostname_lookup --master=zk://$IP:2181/mesos --containerizers=docker --port=$slave_start_port --work_dir=$SLAVE_DIR --resources="$SLAVE_RESOURCES";ports(*):[$slave_resource_start_port-$slave_resource_end_port]
 EOF
-  let slave_port=slave_port+1000
+  let slave_start_port=slave_start_port+num_ports
 done
 # XXX don't get it :S i am unable to limit that on our portrange...it's been too long
 iptables -t nat -A POSTROUTING  -j MASQUERADE
--- start-cluster.sh
+++ start-cluster.sh
@@ -1,21 +1,47 @@
 #!/bin/sh
 
 if [ "$#" -lt 1 ]; then
-  echo "Usage: $0 <mesos-cluster-workdir> [--num-slaves=<num-slaves>] [<additional-docker-options>]"
+  echo "Usage: $0 <mesos-cluster-workdir> [--num-slaves=<num-slaves>] [--port-range=<port-range>] [<additional-docker-options>]"
   exit 99
 fi
+
+# ss -ltn sport ge :1024 and sport lt :61123
 
 WORKDIR=$1
 NUM_SLAVES=3
 
 shift
 
-case $1 in 
-  --num-slaves=*)
-  NUM_SLAVES="${1#*=}"
-  shift
-  ;;
-esac
+
+while true; do
+  case $1 in 
+    --num-slaves=*)
+    NUM_SLAVES="${1#*=}"
+    shift
+    ;;
+    --port-range=*)
+    PORT_RANGE="${1#*=}"
+    shift
+    ;;
+    *)
+    break
+  esac
+done
+
+let PORTCOUNT=1000
+let STARTPORT=10000
+
+while [ -z "$PORT_RANGE" ]; do
+  let ENDPORT=$STARTPORT+$PORTCOUNT
+  let NUM_LISTENERS=$(ss -t -l -n "sport gt $STARTPORT and sport le $ENDPORT" | tail -n +2 | wc -l)
+  if [ "$NUM_LISTENERS" -eq 0 ]; then
+    PORT_RANGE=$STARTPORT:$ENDPORT
+  fi
+  let STARTPORT=$STARTPORT+$PORTCOUNT
+  if [ "$ENDPORT" -ge 65335 ]; then
+    exit 2
+  fi
+done
 
 DOCKER_OPTS=$@
 
@@ -25,4 +51,4 @@ fi
 
 DOCKER_SOCKET=${DOCKER_SOCKET:-'/var/run/docker.sock'}
 
-docker -H unix://"$DOCKER_SOCKET" run --privileged -v "$DOCKER_SOCKET":"$DOCKER_SOCKET" -v "$WORKDIR":"$WORKDIR" $@ m0ppers/mesos-cluster $NUM_SLAVES $HOST_IP /data/mesos-cluster
+docker -H unix://"$DOCKER_SOCKET" run --privileged -v "$DOCKER_SOCKET":"$DOCKER_SOCKET" -v "$WORKDIR":"$WORKDIR" $@ m0ppers/mesos-cluster $NUM_SLAVES $HOST_IP $PORT_RANGE /data/mesos-cluster
